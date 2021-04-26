31145925dd7e1925e539c3127cb93e0aeee5e214
Ahmed Elhadi <a.alhadi@ad.net.sa>
2021-04-26 15:02:05 +0300

    test  bg image

--- resources/views/emails/newsletter.blade.php
+++ resources/views/emails/newsletter.blade.php
@@ -344,13 +344,13 @@
                                             <table cellpadding="0" cellspacing="0" width="100%"
                                                 style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px">
                                                 <tr>
-                                                    <td align="center" valign="top"
+                                                    <td align="center" valign="top" 
                                                         style="padding:0;Margin:0;width:600px">
                                                         <table cellpadding="0" cellspacing="0" width="100%"
                                                             role="presentation"
-                                                            style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;background: url(https://ad.net.sa/themes/adtheme/img/animation/Old-Logo.svg);background-size: cover;">
+                                                            style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;background: url(//ad.net.sa/themes/adtheme/img/animation/Old-Logo.svg);background-size: cover;">
                                                             <tr>
-                                                                <td align="center" style="padding:0;Margin:0">
+                                                                <td align="center" style="padding:0;Margin:0" background="{{ asset('images/bg-light.png') }}">
                                                                     <p
                                                                         style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:38px;color:#231F20;font-size:25px">
                                                                         <strong>{{ $article->subtitle }}</strong>
