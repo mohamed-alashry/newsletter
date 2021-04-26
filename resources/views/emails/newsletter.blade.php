c0a32f33d9870afd0009407f2a133606f86df5e9
Ahmed Elhadi <a.alhadi@ad.net.sa>
2021-04-26 15:12:32 +0300

    Update newsletter.blade.php

--- resources/views/emails/newsletter.blade.php
+++ resources/views/emails/newsletter.blade.php
@@ -348,8 +348,8 @@
                                                         style="padding:0;Margin:0;width:600px">
                                                         <table cellpadding="0" cellspacing="0" width="100%"
                                                             role="presentation"
-                                                            style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;background: url(//ad.net.sa/themes/adtheme/img/animation/Old-Logo.svg);background-size: cover;" background="{{ asset('images/bg-light.png') }}">
-                                                            <tr>
+                                                            style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;background: url(//ad.net.sa/themes/adtheme/img/animation/Old-Logo.svg);background-size: cover;" >
+                                                            <tr background="{{ asset('images/bg-light.png') }}">
                                                                 <td align="center" style="padding:0;Margin:0" >
                                                                     <p
                                                                         style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:38px;color:#231F20;font-size:25px">
